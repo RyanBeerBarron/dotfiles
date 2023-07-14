1c8501f9891b273a3b947f3ffce393ce65f83455
Ryan Barron <ryanbar1997@gmail.com>
2023-07-15 00:57:12 +0200

    Clean and polish all the bash functions and other

--- .local/share/bash/lib.bash
+++ .local/share/bash/lib.bash
@@ -1,56 +1,61 @@
 . $XDG_DATA_HOME/sh/man-pattern.sh
-# . git-pattern.sh
 . $XDG_DATA_HOME/sh/ANSI.sh
 
+# Should probably be a script instead of a function
+# so I can use it with Perl / Python
+# But only useful for WSL, so I will soon ditch this(?)
 create_alacritty_link () {
-    cd "$WHOME"/AppData/Roaming/alacritty
-    # cmd.exe /c "del alacritty.yml & mklink alacritty.yml $(wslpath -w ~/.config/alacritty/alacritty.yml)" > /dev/null
-    cp -f $HOME/.config/alacritty/alacritty.yml alacritty.yml
-    cd - > /dev/null
+    pushd "$WHOME"/AppData/Roaming/alacritty > /dev/null
+    cp -f $XDG_CONFIG_HOME/alacritty/alacritty.yml alacritty.yml
+    popd > /dev/null
 }
-export -f create_alacritty_link
 
-colorscheme () {
-    if [ "$1" == "list" ]; then
-        awk 'BEGIN {FS=";"} {print $1}' ~/.config/themes;
-        return;
+# terminal config
+tconf () {
+    nvim ~/.config/alacritty/alacritty.yml
+    create_alacritty_link
+}
+
+# Function to modify the color of alacritty
+chcolor () {
+    local FILE=${XDG_CONFIG_HOME:-$HOME/.config/}"/themes"
+    if test "$1" = "list"; then
+        cut -d';' -f1 $FILE;
+        return 0;
     fi;
-    if [ -z "$1" ]; then
-        grep --color=auto ";CURRENT_COLORSCHEME" ~/.config/themes | cut --delimiter=";" --fields=1;
+    if test -z "$1"; then
+        sed -En 's/^([[:alnum:]]+);.*CURRENT/\1/p' $FILE;
+        return 0;
     else
-        sed -i 's/;CURRENT_COLORSCHEME//' ~/.config/themes;
-        sed -i "/$1/s/$/;CURRENT_COLORSCHEME/" ~/.config/themes;
-        a=$(grep "$1" ~/.config/themes);
-        b=$(echo $a | cut --delimiter=";" --fields=1);
-        c=$(echo $a | cut --delimiter=";" --fields=2);
-        d=$(echo $a | cut --delimiter=";" --fields=3);
-        if [ ! -z "$c" ]; then
-            sed -Ei "s/background = .*/background = '$c'/" ~/.config/nvim/after/plugin/colors.lua;
-        fi;
-        sed -i "s/colorscheme .*\(\")\|\]\]\)/colorscheme $b\1/" ~/.config/nvim/after/plugin/colors.lua;
-        sed -i "s/^colors: \*.*/colors: *$b/" ~/.config/alacritty/alacritty.yml;
-        BAT_THEME=$d;
-        export BAT_THEME;
+        sed -i.bak 's/;CURRENT.*//' $FILE;
+        sed -i.bak "/$1/s/$/;CURRENT/" $FILE;
+        # Can not use a pipe 'grep | mapfile' because pipe run each part in a subprocess
+        # and so mapfile will create an array in a subprocess which is useless in a bash function
+        mapfile -td";" ARRAY < <(grep "$1" $FILE);
+        sed -i.bak "s/^colors: .*/colors: *${ARRAY[0]}/" $XDG_CONFIG_HOME/alacritty/alacritty.yml
         create_alacritty_link;
+        return 0;
     fi
 }
+complete -W "list $(cut -d";" -f1 ${XDG_CONFIG_HOME:-$HOME/.config/}"/themes")" chcolor
+
 
 start-session () {
     echo "starting session...";
-    tmpfile=$(mktemp /tmp/start-session.script.XXXXXX);
-    local dir=""
-    for dir in $HOME/Work/* ; do
-        for repo in $(find $dir -maxdepth 1 -type d); do
-            if git -C "$repo" rev-parse 2> /dev/null; then
-                git -C "$repo" worktree list | cut -d" " -f1 >> $tmpfile 2> /dev/null;
+    local TMPFILE=$(mktemp /tmp/start-session.script.XXXXXX);
+    local DIR=""
+    for DIR in $HOME/Work/* ; do
+        for REPO in $(find $DIR -maxdepth 1 -type d); do
+            if git -C "$REPO" rev-parse 2> /dev/null; then
+                git -C "$REPO" worktree list | cut -d" " -f1 >> $TMPFILE 2> /dev/null;
             else 
-                echo "$repo" >> $tmpfile 2> /dev/null
+                echo "$REPO" >> $TMPFILE 2> /dev/null
             fi
         done;
     done;
-    echo $HOME >> $tmpfile;
-    TMUX_DIR=$(fzf <$tmpfile);
-    if [ -z "$TMUX_DIR" ]; then
+    echo $HOME >> $TMPFILE;
+    TMUX_DIR=$(sort $TMPFILE | uniq | fzf );
+    if test -z "$TMUX_DIR"; then
         printf "No directory chosen, creating no session. Exiting...\n";
         return;
     fi;
@@ -131,72 +136,74 @@ opacity () {
     fi
 }
 
-dotfiles () {
-    if [ "$1" == "status" ]; then
-        cd $HOME/dotfiles;
-        git status;
-        cd -;
-        return 0;
-    fi
-    # TODO: Try for a rebase still approach
-    # instead of removing '/home/ryan' and concatenating paths
-    # rebase given pathname onto $HOME/dotfiles
-    #
-    # /----home/-----ryan/-----dotfiles/
-    #                    \
-    #                     path/----to/----file/
-    if [ "$1" == "add" ] && [ -e $2 ]; then
-        abs_path=$(get_absolute_path "$2")
-        # cut -d/ -f4- is to remove '/home/#username/...' from the absolute path
-        # 1st field is empty '^' just the start of the line
-        # 2nd field is 'home'
-        # 3rd field is '#username' 
-        # Also append a '/' at the end of the dirname, useful for consistency
-        dirname=$(echo "$(dirname $(readlink -f $2 | cut -d/ -f4-))/");
-        # If the file is in home directory, i.e. dirname is "." after removing /home/#username/
-        # replace it by the empty string, otherwise leave as is //TODO check for a better way to do this?
-        # Since we appended a forward slash, we check for "./" 
-        dirname=$( [ "$dirname" == "./" ] && echo "" || echo $dirname );
-        basename=$(basename $(readlink -f $2 | cut -d/ -f4-));
-        echo "dirname is: $dirname"
-        echo "basename is: $basename"
-        if [ -n "$dirname" ];
-        then
-            echo "mkdir -p $HOME/dotfiles/$dirname"
-            mkdir -p $HOME/dotfiles/$dirname
-        fi
-        echo "cp $2 $HOME/dotfiles/$dirname"
-        cp -f $2 $HOME/dotfiles/$dirname
-        echo "rm $2"
-        rm -f $2
-        echo "ln -s $HOME/dotfiles/${dirname}$basename $2"
-        ln -fs $HOME/dotfiles/${dirname}$basename $2
-        unset basename
-        unset dirname
-        return 0
-    fi
-    echo "1st arg is: $1"
-    echo "2nd arg is: $2"
-    if [ "$1" == "remove" ] && [ -h $2 ]; then
-        echo "hello"
-        dirname=$(echo "$(dirname $(readlink -f $2 | cut -d/ -f4-))/");
-        dirname=$( [ "$dirname" == "./" ] && echo "" || echo $dirname );
-        basename=$(basename $(readlink -f $2 | cut -d/ -f4-));
-        echo "dirname is: $dirname"
-        echo "basename is: $basename"
-        echo "testing for $HOME/dotfiles/$dirname$basename" 
-        if [ -e $HOME/dotfiles/$dirname$basename ]; then
-            rm $2
-            mv $HOME/dotfiles/$dirname$basename $(dirname $2)
-        fi
-        return 0
-    fi
-}
-get_absolute_path () {
+# dotfiles () {
+#     if [ "$1" == "status" ]; then
+#         cd $HOME/dotfiles;
+#         git status;
+#         cd -;
+#         return 0;
+#     fi
+#     # TODO: Try for a rebase still approach
+#     # instead of removing '/home/ryan' and concatenating paths
+#     # rebase given pathname onto $HOME/dotfiles
+#     #
+#     # /----home/-----ryan/-----dotfiles/
+#     #                    \
+#     #                     path/----to/----file/
+#     if [ "$1" == "add" ] && [ -e $2 ]; then
+#         abs_path=$(get_absolute_path "$2")
+#         # cut -d/ -f4- is to remove '/home/#username/...' from the absolute path
+#         # 1st field is empty '^' just the start of the line
+#         # 2nd field is 'home'
+#         # 3rd field is '#username' 
+#         # Also append a '/' at the end of the dirname, useful for consistency
+#         dirname=$(echo "$(dirname $(readlink -f $2 | cut -d/ -f4-))/");
+#         # If the file is in home directory, i.e. dirname is "." after removing /home/#username/
+#         # replace it by the empty string, otherwise leave as is //TODO check for a better way to do this?
+#         # Since we appended a forward slash, we check for "./" 
+#         dirname=$( [ "$dirname" == "./" ] && echo "" || echo $dirname );
+#         basename=$(basename $(readlink -f $2 | cut -d/ -f4-));
+#         echo "dirname is: $dirname"
+#         echo "basename is: $basename"
+#         if [ -n "$dirname" ];
+#         then
+#             echo "mkdir -p $HOME/dotfiles/$dirname"
+#             mkdir -p $HOME/dotfiles/$dirname
+#         fi
+#         echo "cp $2 $HOME/dotfiles/$dirname"
+#         cp -f $2 $HOME/dotfiles/$dirname
+#         echo "rm $2"
+#         rm -f $2
+#         echo "ln -s $HOME/dotfiles/${dirname}$basename $2"
+#         ln -fs $HOME/dotfiles/${dirname}$basename $2
+#         unset basename
+#         unset dirname
+#         return 0
+#     fi
+#     echo "1st arg is: $1"
+#     echo "2nd arg is: $2"
+#     if [ "$1" == "remove" ] && [ -h $2 ]; then
+#         echo "hello"
+#         dirname=$(echo "$(dirname $(readlink -f $2 | cut -d/ -f4-))/");
+#         dirname=$( [ "$dirname" == "./" ] && echo "" || echo $dirname );
+#         basename=$(basename $(readlink -f $2 | cut -d/ -f4-));
+#         echo "dirname is: $dirname"
+#         echo "basename is: $basename"
+#         echo "testing for $HOME/dotfiles/$dirname$basename" 
+#         if [ -e $HOME/dotfiles/$dirname$basename ]; then
+#             rm $2
+#             mv $HOME/dotfiles/$dirname$basename $(dirname $2)
+#         fi
+#         return 0
+#     fi
+# }
+
+abs_path () {
     echo "$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
 }
 export -f __git_ps1
 
+# Last field 
 lf () {
     local USAGE="   
     lf - Last field
@@ -204,6 +211,7 @@ lf () {
     Usage: 
         lf <delimiter>
 "
+    # First argument should be a single char since it is passed to `cut`
     if test ${#1} -ne 1; then
         printf "$USAGE"
         return 1;
@@ -211,16 +219,15 @@ lf () {
     rev | cut -d"$1" -f1 | rev
 }
 conf () {
-    # echo "$XDG_CONFIG_HOME/$1/"*
     if test "$1" == "ssh"; then
         $EDITOR $HOME/.ssh/config
     else
         find "$XDG_CONFIG_HOME/$1/" -type f  | xargs $EDITOR
     fi
 }
-# auto complete for directories ("^d") and using last field which is the directory itself
 
 _conf_comp () {
+    # auto complete for directories ("^d") and using last field which is the directory itself
     local conf_dirs=$(ls -l "$XDG_CONFIG_HOME/" | grep "^d" | lf " ")
     conf_dirs+=" ssh"
     COMPREPLY=($(compgen -W "${conf_dirs[*]}" -- $2))
@@ -229,7 +236,7 @@ complete -F _conf_comp conf
 
 fn () {
     if test -z $1; then
-        nvim $HOME/scripts
+        nvim $FUNCTION_HOME
     elif test -x "$HOME/.local/scripts/$1"; then
         nvim "$HOME/.local/scripts/$1"
     else
@@ -244,12 +251,13 @@ fn () {
 }
 
 _fn_comp () {
-    local fn_list=$(declare -F | cut -d" " -f3 | grep -v "^_.\+$" | lf " " )
+    local fn_list=$(declare -F | cut -d" " -f3 | grep -v "^_.\+$" )
     local script_list=$(find $HOME/.local/scripts/ -executable -type f -exec basename {} \;) 
     fn_list+=("${script_list[@]}")
     COMPREPLY=($(compgen -W "${fn_list[*]}" -- $2))
 }
 complete -F _fn_comp fn
+
 idea () {
     local IDEA_PATH=$HOME/.local/share/JetBrains/Toolbox/scripts
     if test -z $IDEA_PID || ! ps -p $IDEA_PID > /dev/null; then
@@ -268,19 +276,11 @@ curljson () {
 function opt () {
     man $1 | awk '{ print NR "\t" $0 }' | sed -n "/^[[:digit:]]\+\t[[:space:]]\+$2\($\|[[:space:]]\|,\)/,/^[[:digit:]]\+\t$/p"
 }
+
 vis () {
     sed -e "s/ /^S/g" -e "s/\t/^T/g" -e "s/\n/^N/g"
 }
 
-tmux-help () {
-    if test -z "$1"; then
-        echo -e "usage: tmux-help ${STRIKETHROUGH}COMMAND${RESET}" >&2
-        return 1;
-    fi
-     MANWIDTH=160 man tmux |
-     sed -n "/^[[:space:]]\+$1\( \[-[[:alnum:]]\+\]\)/,/^[[:space:]]\+[[:alnum:]-]\+\( \[-[[:alnum:]]\+\]\)\+/p" |
-     head -n -1 
-}
 
 _tmux-help_completion () {
     if test -z "$TMUX_HELP_COMPLETION"; then
