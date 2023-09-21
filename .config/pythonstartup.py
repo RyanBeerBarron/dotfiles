a11e68ff6b379c969522c2a028f36e838e14b11e
Ryan Barron <ryanbar1997@gmail.com>
2023-09-21 20:09:50 +0200

    config(python): Python PS1 prompt showing variables
    This prompt shows global variables and their values.
    It only shows "new" variables, that were introduced from the interactive session.
    This is done by recording the variable in the global scope when this file is first sourced
    and every time the PS1 prompt is refreshed, it compares the current global scope to the recorded one.
    The diff is taken and pretty printed as the prompt
    
    Disclaimer: I barely use Python, probably bad

--- .config/pythonstartup.py
+++ .config/pythonstartup.py
@@ -10,13 +10,40 @@ import atexit
 import os
 import readline
 import sys
+import io
+
+context = globals()
+class Prompt:
+    def __str__(self):
+        context_keys = set(context.keys())
+        self.new_keys = context_keys - fixed
+        self.new_keys.remove('fixed')
+        if not self.new_keys:
+            return 'Global environment: {}\n'
+        else:
+            return self.pretty_print()
+
+    def pretty_print(self):
+        output = io.StringIO()
+        print('Global environment: { ', end='', file=output)
+        for i, key in enumerate(self.new_keys):
+            print(f'{key}: {context[key]}', end='', file=output)
+            if i < len(self.new_keys) - 1:
+                print(", ", end='', file=output)
+        print(' }', end='\n', file=output)
+        contents = output.getvalue()
+        output.close()
+        return contents
+    new_keys = None
 
 histfile = os.path.join(os.getenv("XDG_CACHE_HOME", os.path.expanduser("~/.cache")), "python_history")
 try:
+
     readline.read_history_file(histfile)
-            # default history len is -1 (infinite), which may grow unruly
+    # default history len is -1 (infinite), which may grow unruly
     readline.set_history_length(1000)
-    sys.ps1 = 'hello\n'
+    sys.ps1 = Prompt()
 except FileNotFoundError:
     pass
 atexit.register(readline.write_history_file, histfile)
+fixed = set(globals().keys())
