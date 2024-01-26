c074f8277135af76b823c632dfe18716693725f3
Ryan Barron <ryanbar1997@gmail.com>
2024-01-26 23:22:25 +0100

    (treesitter) disable highlighting for vimdoc
    I had issue where vimdoc keywords would display wrong.
    The bars and asterisks, '|' and '*', would show.
    Instead of hiding those characters and being a different color.
    Disabling treesitter fix this. And I don't use treesitter functionalities with vimdoc

--- .config/nvim/plugin/treesitter.lua
+++ .config/nvim/plugin/treesitter.lua
@@ -2,7 +2,7 @@ require 'nvim-treesitter.configs'.setup({
     sync_install = false,
     highlight = {
         enable = true,
-        disable = { "vim" },
+        disable = { "vim", "vimdoc" },
         additional_vim_regex_highlighting = false
     },
 
