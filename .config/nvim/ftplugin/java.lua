82a286154bbf260fbc145efc33d8a82862a72229
Ryan Barron <ryanbar1997@gmail.com>
2024-04-26 09:45:20 +0200

    (nvim) Add 'Cmp' package for completion and snippets from 'vsnip'

--- .config/nvim/ftplugin/java.lua
+++ .config/nvim/ftplugin/java.lua
@@ -12,6 +12,9 @@ local capabilities = {
     }
 }
 
+local cmp_capabilities = require'cmp_nvim_lsp'.default_capabilities()
+capabilities = vim.tbl_extend("keep", capabilities, cmp_capabilities)
+
 local function tagfunc(pattern)
     return vim.lsp.tagfunc(pattern, "c")
 end
