8331b52db5eb7410f9a140228986ba0dfc209931
Ryan Barron <ryanbar1997@gmail.com>
2024-05-13 00:10:08 +0200

    (nvim) Add new packages 'Hydra.nvim' and 'marks.nvim'
    Hydra plugin is used mainly as vim-unimpaired but with single key mappings.
    Can use ']' and '[' for all buffers, args, quickfix entry, etc...
    
    Marks is used for having visible marks in the signcolumn and keybinds to add/delete/cycle them

---
+++ .config/nvim/plugin/hydra.lua
@@ -0,0 +1,38 @@
+local Hydra = require'hydra'
+local utils = require'utils'
+
+Hydra({
+    name = "Side scroll",
+    mode = "n",
+    body = "z",
+    heads = {
+        { 'h', 'zh' },
+        { 'j', '<C-e>' },
+        { 'k', '<C-y>' },
+        { 'l', 'zl', { desc = '←/→' }},
+        { 'H', 'zH' },
+        { 'L', 'zL', { desc = 'half screen ←/→' }},
+        { 'q', nil, { exit = true, desc = "exit" }},
+    }
+})
+
+Hydra({
+    name = "Window resize",
+    mode = "n",
+    body = "<C-w>",
+    heads = {
+        { '+', '<c-w>+' },
+        { '-', '<c-w>-' },
+        { '>', '<c-w>>' },
+        { '<', '<c-w><' },
+        { 'q', nil, { exit = true, desc = "exit" }},
+    }
+})
+
+utils.createHydra( { name = "Arg", key = "a", next = "next", prev = "prev", del = "argdelete" } )
+utils.createHydra( { name = "Buffer", key = "b", next = "bnext", prev = "bprev", del = "bdelete" } )
+utils.createHydra( { name = "Tabs", key = "t", next = "tabnext", prev = "tabprev", del = "tabclose" } )
+utils.createHydra( { name = "Quickfix", key = "q", next = "cnext", prev = "cprev" } )
+utils.createHydra( { name = "Location", key = "l", next = "lnext", prev = "lprev" } )
+utils.createHydra( { name = "Tags", key = "g", next = "tag", prev = "pop" } )
+utils.createHydra( { name = "Marks", key = "m", next = "lua require'marks'.next()", prev = "lua require'marks'.prev()", del = "lua require'marks'.delete_line()"})
--- .config/nvim/plugin/packages_setup.lua
+++ .config/nvim/plugin/packages_setup.lua
@@ -42,4 +42,14 @@ require('lualine').setup {
 require('kanagawa').setup({
     undercurl = false,
 })
+require'marks'.setup {
+  -- whether to map keybinds or not. default true
+  default_mappings = true,
+  mappings = {
+    next = false,
+    prev = false
+    }
+}
+
+
 vim.cmd("colo kanagawa")
